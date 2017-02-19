df257b81f52b1cfbe3d2d573e7efc537ee913f04
Аня <@LAPTOP-9UT6AG7R>
2017-02-19 20:59:25 +0500

    Triangle

--- NewRepo/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ NewRepo/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,20 +10,44 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            
-            Random Gen = new Random();
+
+            Random gen = new Random();
             Edge[] edges = new Edge[3];
             Point[] points = new Point[3];
+            Triangle triangle = new Triangle(points);
+            double perimeter = 0;
+            double area = 0;
 
             for (int i = 0; i < 3; i++)
             {
-                points[i] = new Point(Gen.Next(-5, 5), Gen.Next(-5, 5));
+                points[i] = new Point(gen.Next(-5, 5), gen.Next(-5, 5));
                 points[i].PrintCoordinate();
-                
+
             }
-            
-            
-            
+            for (int i = 0; i < edges.Length; i++)
+            {
+                if (i < edges.Length - 1)
+                {
+                    edges[i] = new Edge(points[i], points[i + 1]);
+
+                }
+                else edges[i] = new Edge(points[i], points[0]);
+                Console.WriteLine("ребро {0}={1}", i, edges[i].Lenght()); //подписать ребро такое то и тд
+            }
+            if (triangle.Right(edges) == true)
+            {
+                Console.WriteLine("Прямоугольный");
+                perimeter = triangle.Perimeter(edges, perimeter);
+                Console.WriteLine("Периметер " + perimeter);
+            }
+            if (triangle.Isosceles(edges) == true)
+            {
+                Console.WriteLine("Равнобедренный");
+                perimeter = triangle.Perimeter(edges, perimeter);
+                area = triangle.Area(edges, perimeter, area);
+                Console.WriteLine("Площадь "+area);
+            }
+
             Console.ReadLine();
 
         }
--- NewRepo/ConsoleApplication1/ConsoleApplication1/Triangle.cs
+++ NewRepo/ConsoleApplication1/ConsoleApplication1/Triangle.cs
@@ -12,14 +12,66 @@ namespace ConsoleApplication1
                 
         public Triangle(Point[] points)
         {
-            this.points = points;
+            this.points = new Point[points.Length];
             for (int i = 0; i < points.Length; i++)
             {
                 this.points[i] = points[i];
             }
         }
-        
 
-        
+        public double Perimeter(Edge[] edges, double perimeter)
+        {
+            for (int i = 0; i < edges.Length; i++)
+            {
+                double edgeTriangle = edges[i].Lenght();//вызывается функция из edge  и находится длина
+                perimeter += edgeTriangle;
+                
+            }
+            return perimeter;
+        }
+
+        public double Area(Edge[] edges, double perimeter, double area)
+        {
+            //area = корень(p*(p-a)*(p-b)*(p-c))
+            double polPerimeter = perimeter / 2;
+            double time;                //разность полупериметра и стороны
+            double proiztimes=1;          //произведение временных переменных
+            for (int i = 0; i < edges.Length; i++)
+            {
+                time = polPerimeter - edges[i].Lenght();//функция
+                proiztimes *= time;
+
+            }
+            area = Math.Sqrt(proiztimes * polPerimeter);
+            return area;
+        }
+        public bool Right(Edge[] edges)
+        {
+            if (edges[0].Lenght() == Math.Sqrt(Math.Pow(edges[1].Lenght(), 2) + Math.Pow(edges[2].Lenght(), 2)))
+            {
+                return true;
+            }
+            if (edges[1].Lenght() == Math.Sqrt(Math.Pow(edges[2].Lenght(), 2) + Math.Pow(edges[0].Lenght(), 2)))
+            {
+                return true;
+            }
+            if (edges[2].Lenght() == Math.Sqrt(Math.Pow(edges[0].Lenght(), 2) + Math.Pow(edges[1].Lenght(), 2)))
+            {
+                return true;
+            }
+            else return false;
+        }
+        public bool Isosceles(Edge[] edges)
+        {
+            if (edges[0] == edges[1] || edges[1] == edges[2] || edges[2] == edges[0])
+            {
+                return true;
+            }
+            else return false;
+        }
+
+
+
+
     }
 }
