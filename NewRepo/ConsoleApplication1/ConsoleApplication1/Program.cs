912efb46c3d15f2bfce47d9279ea5bc45b562709
Аня <@LAPTOP-9UT6AG7R>
2017-02-19 21:36:16 +0500

    Triangle2_0

--- NewRepo/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ NewRepo/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,20 +10,66 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество треугольников ");
+            int countTriangle = Convert.ToInt32(Console.ReadLine());
 
             Random gen = new Random();
             Edge[] edges = new Edge[3];
             Point[] points = new Point[3];
-            Triangle triangle = new Triangle(points);
             double perimeter = 0;
             double area = 0;
+            Triangle[] triangles = new Triangle[countTriangle];
+            double averagePerimeter=0;
+            double averageArea=0;
+            int countRight = 0;
+            int countIsosceles = 0;
 
+            for (int i = 0; i < triangles.Length; i++)
+            {
+
+                RandomCoordinatesPoints(gen, points, triangles, i);
+                EdgesLenght(edges, points);
+
+                if (triangles[i].Right(edges) == true)
+                {
+                    Console.WriteLine("Прямоугольный");                    
+                    countRight++;
+                    perimeter = triangles[i].Perimeter(edges, perimeter);
+                    Console.WriteLine("Периметер " + perimeter);
+                    averagePerimeter += perimeter;
+                }
+                if (triangles[i].Isosceles(edges) == true)
+                {
+                    Console.WriteLine("Равнобедренный");                    
+                    countIsosceles++;
+                    perimeter = triangles[i].Perimeter(edges, perimeter);
+                    area = triangles[i].Area(edges, perimeter, area);
+                    Console.WriteLine("Площадь " + area);
+                    averageArea += area;
+                }
+                Console.WriteLine();
+            }
+            averagePerimeter = averagePerimeter / countRight;
+            averageArea = averageArea / countIsosceles;
+            Console.WriteLine("Количество прямоуголных = {0}, их среднее = {1}", countRight, averagePerimeter);
+            Console.WriteLine("Количество равнобедренных = {0}, их среднее = {1}", countIsosceles, averageArea);
+
+            Console.ReadLine();
+
+        }
+        public static void RandomCoordinatesPoints(Random gen, Point[] points, Triangle[] triangles, int numberTriangle) //numberTriangle - номер треугольника
+        {
             for (int i = 0; i < 3; i++)
             {
-                points[i] = new Point(gen.Next(-5, 5), gen.Next(-5, 5));
+                points[i] = new Point(gen.Next(0, 5), gen.Next(0, 5));
                 points[i].PrintCoordinate();
+                
 
             }
+            triangles[numberTriangle] = new Triangle(points);
+        }
+        public static void EdgesLenght(Edge[] edges, Point[] points)
+        {
             for (int i = 0; i < edges.Length; i++)
             {
                 if (i < edges.Length - 1)
@@ -34,25 +80,7 @@ namespace ConsoleApplication1
                 else edges[i] = new Edge(points[i], points[0]);
                 Console.WriteLine("ребро {0}={1}", i, edges[i].Lenght()); //подписать ребро такое то и тд
             }
-            if (triangle.Right(edges) == true)
-            {
-                Console.WriteLine("Прямоугольный");
-                perimeter = triangle.Perimeter(edges, perimeter);
-                Console.WriteLine("Периметер " + perimeter);
-            }
-            if (triangle.Isosceles(edges) == true)
-            {
-                Console.WriteLine("Равнобедренный");
-                perimeter = triangle.Perimeter(edges, perimeter);
-                area = triangle.Area(edges, perimeter, area);
-                Console.WriteLine("Площадь "+area);
-            }
-
-            Console.ReadLine();
-
         }
-        
-        
     }
 }
 
--- NewRepo/ConsoleApplication1/ConsoleApplication1/Triangle.cs
+++ NewRepo/ConsoleApplication1/ConsoleApplication1/Triangle.cs
@@ -69,6 +69,10 @@ namespace ConsoleApplication1
             }
             else return false;
         }
+        public void VerificationPoints()
+        {
+
+        }
 
 
 
